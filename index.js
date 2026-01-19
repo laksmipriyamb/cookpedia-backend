63bbca7d3ae70fcafd3c0e6cd9f5317fe2fb4290
lakshmipriyamb <lakshmipriyamb2002@gmail.com>
2026-01-19 13:09:35 +0530

    cookpedia-backend v1

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+require('./config/db')
+const routes = require('./routes/routing')
+
+const cookpediaServer = express()
+
+cookpediaServer.use(cors())
+cookpediaServer.use(express.json())
+cookpediaServer.use(routes)
+
+const PORT = 3000
+
+cookpediaServer.listen(PORT,()=>{
+    console.log("Cookpedia Server Started...Waiting for client request");
+    
+})
+
+cookpediaServer.get('/',(req,res)=>{
+    res.status(200).send('<h1>Cookpedia Server Started...Waiting for client request</h1>')
+})
